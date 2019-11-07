ea3319a5e5aacd90d136cd9e2d52b8793912e4d1
“anstarky” <anstarky@gmail.com>
2019-11-07 13:05:29 +0200

    Updates

---
+++ precache-manifest.7652096971220f7a5d87ca1b5869f6de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7467c0a710bd26d83bc61bcaf71dd3e",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "51f50e44433f105f2829",
+    "url": "/goit-react-hw-02-reader/static/css/main.523d27ed.chunk.css"
+  },
+  {
+    "revision": "7424b3ef71d101da6e52",
+    "url": "/goit-react-hw-02-reader/static/js/2.4706995b.chunk.js"
+  },
+  {
+    "revision": "51f50e44433f105f2829",
+    "url": "/goit-react-hw-02-reader/static/js/main.b11bba97.chunk.js"
+  },
+  {
+    "revision": "46b5d60be30d4a7b06da",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.7bf316d8.js"
+  }
+]);
